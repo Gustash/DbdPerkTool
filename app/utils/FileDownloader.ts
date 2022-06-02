9291451a940825ec4628ca101194b99a308a3a1b
Nick Crast <nrcrast@gmail.com>
2022-06-01 21:00:30 -0400

    Fixing download issues

--- app/utils/FileDownloader.ts
+++ app/utils/FileDownloader.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import fetch from 'node-fetch';
 import log from 'electron-log';
+import { Agent } from 'https';
 
 export class FileDownloader {
     constructor(private fsLocation: string, private url: string) { }
@@ -11,36 +12,40 @@ export class FileDownloader {
             throw new Error('Unable to open write stream');
         }
 
-        const resp = await fetch(this.url, {compress: true});
+        const agent = new Agent({
+            rejectUnauthorized: false,
+        });
+
+        const resp = await fetch(this.url, {agent, compress: true});
 
         return new Promise<void>((res, reject) => {
-            const contentLength = parseInt(resp.headers.get('Content-Length') ?? '0', 10);
-            let receivedLength = 0;
-            let lastReportedProgressPct = 0;
-            resp.body.on('readable', async () => {
-                let chunk;
-                while (null !== (chunk = resp.body.read())) {
-                    receivedLength += chunk.length;
-                    stream.write(chunk);
-                    const progress = (receivedLength * 100) / contentLength;
-                    if (progress - lastReportedProgressPct > 1) {
-                        lastReportedProgressPct = progress;
-                        onProgress(progress);
-                        log.info(`Received ${receivedLength} of ${contentLength} (${progress})`);
+                const contentLength = parseInt(resp.headers.get('Content-Length') ?? '0', 10);
+                let receivedLength = 0;
+                let lastReportedProgressPct = 0;
+                resp.body.on('readable', async () => {
+                    let chunk;
+                    while (null !== (chunk = resp.body.read())) {
+                        receivedLength += chunk.length;
+                        stream.write(chunk);
+                        const progress = (receivedLength * 100) / contentLength;
+                        if (progress - lastReportedProgressPct > 1) {
+                            lastReportedProgressPct = progress;
+                            onProgress(progress);
+                            log.info(`Received ${receivedLength} of ${contentLength} (${progress})`);
+                        }
                     }
-                }
-            });
-            resp.body.on('end', () => {
-                log.info('Download complete');
-                stream?.close();
-                res();
-            });
+                });
+                resp.body.on('end', () => {
+                    log.info('Download complete');
+                    stream?.close();
+                    res();
+                });
 
-            resp.body.on('error', (e: Error) => {
-                log.warn('Download complete with error', e);
-                stream?.close();
-                reject(e);
+                resp.body.on('error', (e: Error) => {
+                    log.warn('Download complete with error', e);
+                    stream?.close();
+                    reject(e);
+                });
             });
-        });
     }
 }
